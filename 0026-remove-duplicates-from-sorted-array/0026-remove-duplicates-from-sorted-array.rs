03ae5b0deab989cb41b972192fbfe295aba4aaf2
Fábio Barros <58121122+fabio-barros@users.noreply.github.com>
2023-10-09 23:35:52 -0300

    Time: 0 ms (100%), Space: 2.2 MB (64.41%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.rs
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.rs
@@ -1,6 +1,19 @@
 impl Solution {
     pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-        nums.dedup();
-        nums.len() as i32
+        let len = nums.len();
+        if len == 0 {
+            return 0;
+        }
+
+        let mut k = 1;
+
+        for i in 1..len {
+            if nums[i] != nums[i - 1] {
+                nums[k] = nums[i];
+                k += 1;
+            }
+        }
+
+        k as i32
     }
 }
