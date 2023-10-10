f1c0b059fe81d150f08a82f8d04341097c4fafa3
Fábio Barros <58121122+fabio-barros@users.noreply.github.com>
2023-10-09 22:57:35 -0300

    Time: 134 ms (68.21%), Space: 46.4 MB (75.72%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -3,19 +3,23 @@ using System.Linq;
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
 
-        if (nums.Length == 0)
+    if (nums.Length == 0)
         return 0;
 
-        int k = 1;
-        for (int i = 1; i < nums.Length; i++)
-        {
-            if (nums[i] != nums[i - 1])
-            {
-                nums[k++] = nums[i];
-            }
-        }
+    int k = 1;
+    int prev = nums[0]; 
 
-        return k;
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] != prev)
+        {
+            nums[k] = nums[i]; 
+            prev = nums[i]; 
+            k++;
+        }
+    }
+
+    return k;
         
     }
 }
