bc910ba9c7362ccd22c416372cbbf60ba8f1ab02
Fábio Barros <58121122+fabio-barros@users.noreply.github.com>
2023-10-09 23:12:14 -0300

    Time: 61 ms (69.39%), Space: 44.9 MB (29.6%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -7,7 +7,7 @@ var removeDuplicates = function(nums) {
 
     let k = 1; 
     for (let i = 1; i < nums.length; i++) {
-        if (nums[i] !== nums[k - 1]) {
+        if (nums[i] !== nums[i - 1]) {
             nums[k] = nums[i]; 
             k++;
         }
